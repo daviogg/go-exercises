9da9031d5d0191f47a0ec57620840350eba79d73
Davide Oggioni <33121159+daviogg@users.noreply.github.com>
2020-04-12 22:50:37 +0200

    Add upload section

--- WebApp/main.go
+++ WebApp/main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"database/sql"
 	"html/template"
+	"io"
 	"io/ioutil"
 	"net/http"
+	"os"
 	"strconv"
 	"time"
 
@@ -18,10 +20,11 @@ type Page struct {
 }
 
 var (
-	tmplView = template.Must(template.New("test").ParseFiles("base.html", "test.html", "index.html"))
-	tmplEdit = template.Must(template.New("edit").ParseFiles("base.html", "edit.html", "index.html"))
-	db, _    = sql.Open("sqlite3", "cache/web.db")
-	createDB = "create table if not exists pages (title text, body blob, timestamp text)"
+	tmplView   = template.Must(template.New("test").ParseFiles("base.html", "test.html", "index.html"))
+	tmplEdit   = template.Must(template.New("edit").ParseFiles("base.html", "edit.html", "index.html"))
+	tmplUpload = template.Must(template.New("upload").ParseFiles("base.html", "upload.html", "index.html"))
+	db, _      = sql.Open("sqlite3", "cache/web.db")
+	createDB   = "create table if not exists pages (title text, body blob, timestamp text)"
 )
 
 func (p *Page) saveCache() error {
@@ -97,10 +100,51 @@ func save(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/test/"+title, http.StatusFound)
 }
 
+func upload(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		title := "Upload"
+		p := &Page{Title: title}
+		tmplUpload.ExecuteTemplate(w, "base", p)
+	case "POST":
+		err := r.ParseMultipartForm(100000)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		m := r.MultipartForm
+		files := m.File["myfiles"]
+		for i := range files {
+			file, err := files[i].Open()
+			defer file.Close()
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			f, err := os.Create("./files/" + files[i].Filename)
+			defer f.Close()
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			if _, err := io.Copy(f, file); err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			http.Redirect(w, r, "/files/"+files[i].Filename, http.StatusFound)
+
+		}
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+}
+
 func main() {
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	http.Handle("/files/", http.StripPrefix("/files/", http.FileServer(http.Dir("files"))))
+
 	http.HandleFunc("/test/", view)
 	http.HandleFunc("/edit/", edit)
 	http.HandleFunc("/save/", save)
+	http.HandleFunc("/upload/", upload)
 	http.ListenAndServe(":8000", nil)
 }
